05edcbaf342ac5ede45edfe85a8b6d87551c9afe
Steven Campbell <steven.campbell@skiff.com>
2023-05-15 17:14:54 +0000

    hi

--- Learning Blazor/Startup.cs
+++ Learning Blazor/Startup.cs
@@ -7,31 +7,25 @@ using System.Threading.Tasks;
 
 namespace Learning_Blazor.Client
 {
-    public class Startup
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
     {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5001") });
-        }
-
-        public async Task ConfigureAsync(WebAssemblyHostBuilder builder)
-        {
-            builder.RootComponents.Add<Learning_Blazor.App>("app");
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["BaseAddress"]) });
-            await builder.Build().RunAsync();
-        }
-
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-                endpoints.MapFallbackToFile("index.html");
-            });
-        }
+        services.AddScoped<HttpClient>(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5001") });
     }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+            endpoints.MapFallbackToFile("index.html");
+        });
+    }
+}
+
 }
