d9e907bc0790ef7f932e43eca3b2d8c9abcbca9c
Steven Campbell <steven.campbell@skiff.com>
2023-05-15 15:58:53 +0000

    HTTTT

--- Learning Blazor/Startup.cs
+++ Learning Blazor/Startup.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Net.Http;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System.Threading.Tasks;
@@ -8,7 +10,7 @@ namespace MyBlazorWasmApp.Client
     {
         public async Task ConfigureServices(WebAssemblyHostBuilder builder)
         {
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             // Other services
         }
 
