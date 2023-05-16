da061293e124445ab7533682bd1edfa25c829b84
Steven Campbell <steven.campbell@skiff.com>
2023-05-16 05:48:30 +0000

    jik

--- Learning Blazor/Program.cs
+++ Learning Blazor/Program.cs
@@ -8,19 +8,21 @@ using System.Threading.Tasks;
 
 namespace Learning_Blazor.Client
 {
-    public class Program
+public class Program
+{
+    public static async Task Main(string[] args)
     {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("randomNumberAPI"));
 
-            builder.Logging.ClearProviders();
-            builder.Logging.AddConsole();
+        builder.Logging.ClearProviders();
+        builder.Logging.AddConsole();
 
-            await builder.Build().RunAsync();
-        }
+        await builder.Build().RunAsync();
     }
+}
+
 }
